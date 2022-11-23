374d71bde6e37429e59a00282d166ea408a08255
shawn <shawn@shawndeMacBook-Pro.local>
2022-11-23 18:33:39 +0800

    feature pr4 bugs

--- hellocp.go
+++ hellocp.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
     password := "1234ll5"
     fmt.Println("Hello, World!")
+    var target, num = -5, 3
+
+    target =- num  // Noncompliant; target = -3. Is that really what's meant?
+    target =+ num // Noncompliant; target = 3
 }
